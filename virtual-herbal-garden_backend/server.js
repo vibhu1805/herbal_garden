ecb1f2a4c485360b0751062a7d164cd6870147a2
Vibhuti Sharma <109684722+vibhu1805@users.noreply.github.com>
2024-09-23 21:47:07 +0530

    Update server.js

--- virtual-herbal-garden_backend/server.js
+++ virtual-herbal-garden_backend/server.js
@@ -125,6 +125,7 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+      credentials: true,                // Allow credentials (cookies or authentication)
 }));
 
 app.use(bodyParser.json());
