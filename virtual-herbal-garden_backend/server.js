0da219ed8bc07ed5d064289ee14d2beee7652b7a
Vibhuti Sharma <109684722+vibhu1805@users.noreply.github.com>
2024-09-23 22:37:10 +0530

    Update server.js

--- virtual-herbal-garden_backend/server.js
+++ virtual-herbal-garden_backend/server.js
@@ -151,7 +151,7 @@ app.use((req, res) => {
   res.status(404).json({ message: `Not Found - ${req.originalUrl}` });
 });
 
-const PORT = process.env.BACKEND_URL || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
